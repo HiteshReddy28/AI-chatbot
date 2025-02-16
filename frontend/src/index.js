8e16adcb7d5ff20bd1919cbc33a8402a3b8c1754
HiteshReddy28 <buereddyhiteshreddy28@gmail.com>
2025-02-16 15:11:27 -0500

    Design updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ChatApp from './ChatApp';
+import Login from './login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -13,13 +14,10 @@ root.render(
   <React.StrictMode>
     <Router>
       <div>
-        {/* Navigation Links (Optional) */}
-        
-
-        {/* Define Routes */}
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<App/>} />
           <Route path="/ChatApp" element={<ChatApp/>} />
+          <Route path="/login" element ={<Login/>}/>
         </Routes>
       </div>
     </Router>
--- frontend/src/navbar.js
+++ frontend/src/navbar.js
@@ -3,7 +3,7 @@ import "./App.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="logo">Ai Negotiator</h1>
+      <h1 className="logo">Cognute AI</h1>
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
